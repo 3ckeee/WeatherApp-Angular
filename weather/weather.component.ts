926b82afb687bcbb2b2ac8b995071158467952d7
Erik Kokinda <erikkokinda@Eriks-MacBook-Pro.local>
2022-09-19 12:04:39 +0200

    initial commit

---
+++ weather/weather.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Weather } from '../weather-forecast';
+import { WeatherService } from '../weather.service';
+import { Observable } from 'rxjs';
+import { weatherModel } from '../models/weather.model'
+import { HttpResponse } from '@angular/common/http';
+
+
+
+@Component({
+  selector: 'wa-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent implements OnInit {
+  myWeather!: Weather;
+  location!: GeolocationCoordinates;
+  constructor(private ws:WeatherService) { }
+
+  ngOnInit(): void {
+   this.myWeather = this.ws.weatherNow();
+   navigator.geolocation.getCurrentPosition((pos) => {
+    this.location = pos.coords;
+    const lat = this.location.latitude;
+    const lon = this.location.longitude;
+    this.ws.localWeather(lat, lon).subscribe(Response) 
+ 
+   }
+
+}
